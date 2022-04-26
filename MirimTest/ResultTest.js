1cbd2f091a44ce06e9761c3bf140d4bc508f82f7
Kim Yu Na <yunasun0920@gmail.com>
2022-04-26 22:32:46 +0900

    이미지 조정

--- MirimTest/ResultTest.js
+++ MirimTest/ResultTest.js
@@ -2,7 +2,7 @@
 
 var mirim_percent = localStorage.getItem('mirim_percent');
 
-let image = "../image/ResultTest_"; // 이미지 경로
+let image = "../image/"; // 이미지 경로
 let image_alt = "미림인 테스트"; // 이미지가 나올경우 대체 텍스트
 
 ShowImage();
@@ -15,15 +15,24 @@ function ShowImage(){
 
     switch(mirim_percent / 10){
         case 10: 
-        case 9: result_bg.innerHTML = '<img src="../image/stu_100%.png">'; break;
+        case 9: image += 'stu_100%.png'; break;
         case 8: 
-        case 7: result_bg.innerHTML = '<img src="../image/stu_80%.png">'; break;
+        case 7: image += 'stu_80%.png'; break;
         case 6: 
-        case 5: result_bg.innerHTML = '<img src="../image/stu_60%.png">'; break;
+        case 5: image += 'stu_60%.png'; break;
         case 4: 
-        case 3: result_bg.innerHTML = '<img src="../image/stu_40%.png">'; break;
+        case 3: image += 'stu_40%.png'; break;
         case 2: 
-        case 1: result_bg.innerHTML = '<img src="../image/stu_20%.png">'; break;
-        case 0: result_bg.innerHTML = '<img src="../image/stu_0%.png">'; break;
+        case 1: image += 'stu_20%.png'; break;
+        case 0: image += 'stu_0%.png'; break;
     }
+
+    // img 태그 추가
+    var img = document.createElement('img');
+    img.src = image;
+    img.style.width = '370px';
+    img.style.marginTop = '100px';
+    img.style.marginLeft = '500px';
+
+    result_bg.appendChild(img);
 }
