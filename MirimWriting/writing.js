40f4a3c9e569c0957ea1101c7b976c08473b3409
U-and-Me <yunasun0920@gmail.com>
2022-06-08 09:32:29 +0900

    입력한 글 출력

--- MirimWriting/writing.js
+++ MirimWriting/writing.js
@@ -1,13 +1,8 @@
-var db_config = require('../index_db');
-var dbconn = db_config.init();
-
-bringData();
+//bringData();
 
 // 전송 버튼 클릭 시 => 화면에 출력, DB에 저장
 function clickMsg(){
     const message = document.getElementById('msg').value;
-
-    console.log(message);
 
     //saveData(message);
     showMessage(message);
@@ -17,21 +12,23 @@ function clickMsg(){
 function showMessage(message){
     var chatView = document.querySelector("#chatView");
 
-    chatView.innerHTML += '<span style=" margin-left:1%; width:68px; font-size:30px;">'+message+'</span>';
+    chatView.innerHTML += '<div style=" width:auto; height: 80px; margin-left:2%; margin-top:1%; font-size:30px; font-weight: 600; line-height: 78px; padding-left:1%; padding-right:1%; background-color:white; border-radius:10px">'+message+'</div>';
+
+    
 }
 
 // DB에 저장
 function saveData(message){
-    dbconn.query('INSERT INTO WRITING(user_write) VALUES("' + message + '")', function(err, results, fiels){
+   /* dbconn.query('INSERT INTO WRITING(user_write) VALUES("' + message + '")', function(err, results, fiels){
         console.log(arguments);
-    });
+    });*/
 }
 
 // 페이지 로딩 시 글짓기 데이터 가져와서 출력
 function bringData(){
     var sql = 'SELECT * FROM WRITING';
 
-    dbconn.query(sql, function(err, results, field){
+    conn.query(sql, function(err, results, field){
         console.log(arguments);
     });
 }
--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-var dbconn = require('./index_db');
+
+const mysql = require('mysql');
+var db_config = require('./.config.json');
+
+var static = require('serve-static');
+var bodyParser = require('body-parser');
 
 var app = express();
+var router = express.Router();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/'));
@@ -12,9 +21,45 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + 'index.html');
 });
 
-app.use('./MirimWriting', require('./MirimWriting/writing'));
+app.use('/MirimWriting', static(path.join(__dirname, 'MirimWriting')));
+app.use('/Mirim TMI', static(path.join(__dirname, 'Mirim TMI')));
 
+
+/*
+// mysql 접속 설정
+const conn = mysql.createConnection({
+    host : db_config.host,
+    port : '3306',
+    user : db_config.user,
+    password : db_config.password,
+    database : db_config.database
+});
+
+conn.connect((err) => {
+    if(err){
+        console.log(err);
+        conn.end();
+        throw err;
+    }else{
+        console.log("DB 접속 성공");
+    }
+});
+
+*/
+
+// 글쓰기 라우팅 함수
+router.route('/process/send').post(function(req, res){
+    console.log('/process/send 호출됨');
+
+    var paramId = req.body.name || req.query.name;
+
+    console.log(paramId);
+
+    
+});
+
+app.use('/', router);
 
 http.createServer(app).listen(3000, function(){
-    //console.log(__dirname);
+   // console.log(__dirname);
 });
